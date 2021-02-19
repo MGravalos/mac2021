e0d1b5cea02cdd5c731a4f5d963e89a6ae9afa72
MGravalos <34173501+MGravalos@users.noreply.github.com>
2021-02-19 20:23:37 +0100

    Actualizamos palindromo añadiendo listaPalindromos

--- Proyecto/palindromo.scala
+++ Proyecto/palindromo.scala
@@ -8,3 +8,11 @@ def palindromo(X: Any): Boolean = {
 println(palindromo(111))
 println(palindromo(135))
 println(palindromo("abba"))
+
+//Buscar los nº que son palindromos entre 1 y X
+def listaPalindromos(X: Int): List[Int] = {
+  for(i <- List.range(1,X+1) if palindromo(i))
+    yield i
+}
+
+println(listaPalindromos(200))
