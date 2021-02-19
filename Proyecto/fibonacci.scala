4a5cc01b9ad7ccd0fa987c0146472f59eb2af5dc
MGravalos <34173501+MGravalos@users.noreply.github.com>
2021-02-19 19:10:34 +0100

    Actualizamos fibonacci para que incluya el valor 0

--- Proyecto/fibonacci.scala
+++ Proyecto/fibonacci.scala
@@ -1,14 +1,17 @@
 def fibonaccirec(x: Int): Int = {
-  if(x == 1){
-    1
+  if(x == 0){
+    0
   }else{
-    if(x == 2){
-      2
+    if(x == 1){
+      1
     }else{
-      fibonaccirec(x-1) + fibonaccirec(x-2)
+      if(x == 2){
+        2
+      }else{
+        fibonaccirec(x-1) + fibonaccirec(x-2)
+      }
     }
   }
 }
-
 
 println(fibonaccirec(10))
